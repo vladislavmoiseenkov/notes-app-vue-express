2f2210c197a58fdec016e97ac7f39e69d64aa8fe
Vlad <vladislavmoiseenkov@gmail.com>
2019-01-04 14:56:43 +0200

    test

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -3,7 +3,7 @@ import store from '@/store'
 
 export default () => {
   return axios.create({
-    baseURL: 'http://127.0.0.1:8081',
+    baseURL: 'https://server-6zpz8etzs.now.sh',
     headers: {
       Authorization: `Bearer ${store.state.auth.token}`
     }
